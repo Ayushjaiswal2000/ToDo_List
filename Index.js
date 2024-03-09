6d39137765bc61ac6473a41483e0809a3df2946e
Ayush jaiswal <ayushjaiswal312@gmail.com>
2024-03-10 00:33:11 +0530

    Add Button

---
+++ Index.js
@@ -0,0 +1,13 @@
+// const input = document.getElementById("input");
+// input.addEventListener('input', handleInputChange);
+// // Define the event handler function
+// function handleInputChange() {
+//     if (input.value !== "") {
+//         input.classList.add('right-image');
+//     } else {
+//         input.classList.remove('right-image');
+//     }
+// }
+
+// Attach the event listener to the input element
+
