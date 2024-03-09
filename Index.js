de6b745cc57057b95af1769457cce42190e3e04b
Ayush jaiswal <ayushjaiswal312@gmail.com>
2024-03-10 01:13:41 +0530

    Added Remove Button

--- Index.js
+++ Index.js
@@ -1,13 +1,19 @@
-// const input = document.getElementById("input");
-// input.addEventListener('input', handleInputChange);
-// // Define the event handler function
-// function handleInputChange() {
-//     if (input.value !== "") {
-//         input.classList.add('right-image');
-//     } else {
-//         input.classList.remove('right-image');
-//     }
-// }
+    const addButton = document.getElementById('button');
+    const inputField = document.getElementById('input');
+    const container = document.querySelector('.container');
 
-// Attach the event listener to the input element
-
+    addButton.addEventListener('click', function () {
+    
+        if (inputField.value !== "") {
+            const newDiv = document.createElement('div');
+            newDiv.className = 'task';
+            newDiv.textContent = inputField.value;
+            const removeBtn=document.createElement("button");
+            removeBtn.className="remove";
+            removeBtn.textContent="Remove";
+            container.appendChild(newDiv);
+            removeBtn.style.marginLeft="auto";
+            newDiv.appendChild(removeBtn);
+            inputField.value = '';
+        }
+    });
