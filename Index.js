4294ed3d24fa799f84fa4d1f9fa90e8d5c7e7083
Ayush jaiswal <ayushjaiswal312@gmail.com>
2024-03-10 10:39:12 +0530

    Function to Add and  Remove Btn

--- Index.js
+++ Index.js
@@ -1,11 +1,13 @@
     const addButton = document.getElementById('button');
     const inputField = document.getElementById('input');
     const container = document.querySelector('.container');
+    let count=0;
 
     addButton.addEventListener('click', function () {
     
         if (inputField.value !== "") {
             const newDiv = document.createElement('div');
+            count++;
             newDiv.className = 'task';
             newDiv.textContent = inputField.value;
             const removeBtn=document.createElement("button");
@@ -15,5 +17,16 @@
             removeBtn.style.marginLeft="auto";
             newDiv.appendChild(removeBtn);
             inputField.value = '';
+
+
+            removeBtn.addEventListener("click", (event) => {
+                const clickedButton = event.currentTarget;
+                const taskToRemove = clickedButton.parentElement;
+                taskToRemove.remove();
+                count--;
+            });
         }
     });
+
+
+ 
