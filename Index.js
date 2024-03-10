44fa63f121cd7a233d64df9db8729868de228c8f
Ayush jaiswal <ayushjaiswal312@gmail.com>
2024-03-10 11:03:33 +0530

    Added Img As SNo

--- Index.js
+++ Index.js
@@ -2,23 +2,34 @@
     const inputField = document.getElementById('input');
     const container = document.querySelector('.container');
     let count=0;
+    
 
     addButton.addEventListener('click', function () {
-    
         if (inputField.value !== "") {
             const newDiv = document.createElement('div');
             count++;
             newDiv.className = 'task';
-            newDiv.textContent = inputField.value;
-            const removeBtn=document.createElement("button");
-            removeBtn.className="remove";
-            removeBtn.textContent="Remove";
-            container.appendChild(newDiv);
+    
+            const circle = document.createElement("img");
+            circle.className = "circle";
+            circle.style.marginRight="20px";
+            circle.src = "Images/noun-circle-1203789.png"; 
+    
+            const textContent = document.createElement("div");
+            textContent.textContent = inputField.value;
+    
+            const removeBtn = document.createElement("button");
+            removeBtn.className = "remove";
+            removeBtn.textContent = "Remove";
             removeBtn.style.marginLeft="auto";
+    
+            newDiv.appendChild(circle);
+            newDiv.appendChild(textContent);
             newDiv.appendChild(removeBtn);
+    
+            container.appendChild(newDiv);
             inputField.value = '';
-
-
+    
             removeBtn.addEventListener("click", (event) => {
                 const clickedButton = event.currentTarget;
                 const taskToRemove = clickedButton.parentElement;
@@ -27,6 +38,6 @@
             });
         }
     });
-
+    
 
  
